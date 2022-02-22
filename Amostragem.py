7c64bb294216675f22eb99a29078fd6df1d49979
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-02-21 23:45:47 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-pop = st.sidebar.number_input('Tamanho da população',key=int, step=1, format='%i')
-conf = st.sidebar.slider('Grau de confiança',min_value=80, max_value=99, step=5, format='%i')
-erro = st.sidebar.number_input('Tamanho da população', key= int, min_value=5, format='%i')
+pop = st.sidebar.number_input('Tamanho da população',key=int, min_value=1, step=1, format='%i')
+conf = st.sidebar.slider('Grau de confiança (%)',min_value=80, max_value=99, step=5, format='%i')
+erro = st.sidebar.number_input('Margem de erro (%)', key= int, min_value=5, format='%i')
 
-st.write("os numeros são ", pop,conf,erro)
+st.header.write("os numeros são ", pop,conf,erro)
