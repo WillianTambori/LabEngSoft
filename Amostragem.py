2c62fe70b5ebffd5a44939be648ce99d49cf5b2b
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-02-21 23:31:23 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-pop = st.sidebar.number_input('Tamanho da população')
+pop = st.sidebar.number_input('Tamanho da população',key=int)
 conf = st.sidebar.slider('Grau de confiança',min_value=80, max_value=99, step=5)
 erro = st.sidebar.number_input('Tamanho da população', key= int, min_value=5)
 
