28029cbe068e6ac8743544f28b116792db8f8b14
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-02-22 10:59:45 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -18,5 +18,6 @@ def main ():
     pop =  st.sidebar.number_input('Tamanho da população',key=int, min_value=1, step=1, format='%i')
     conf = st.sidebar.slider('Grau de confiança (%)',min_value=80, max_value=99, step=5, format='%i')
     erro = st.sidebar.number_input('Margem de erro (%)', key= int, min_value=5, format='%i')
+    st.write("os numeros são ", pop,conf,erro)
 
-st.write("os numeros são ", pop,conf,erro)
+
