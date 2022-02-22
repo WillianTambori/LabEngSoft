900bc5e9c90a28f8240a4493f4310b4d9d63e590
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-02-22 18:53:03 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -15,12 +15,13 @@ def porc(n):
     e = float(n/100)
     return e
 def main ():
+    st.sidebar.title("ADS")
     n =  st.sidebar.number_input('Tamanho da população',key=int, min_value=1, step=1, format='%i')
     z = nivel(st.sidebar.slider('Nivel de confiança (%)',min_value=80, max_value=99, step=5, format='%i'))
     e = porc(st.sidebar.number_input('Margem de erro (%)', key= float, min_value=1.0, step=0.5, format='%f'))
     p = porc(st.sidebar.number_input('estimativa de verdadeira proporção (%)', key= int, min_value=10, value=50, max_value=100, step=10, format='%i'))
     N = (n*(z**2)*p*(1-p))/((e**2)*(n-1)+(z**2)*p*(1-p))
-    st.title("o numero de amostra é")
-    st.metric(label="Valor aferido", value=N, delta="numero de amostras")
+    st.title("O Calculo do tamanho da amostra ")
+    st.metric(label="Valor aferido", value=int(N), delta="numero de amostras")
 main()
 
