b6a37f0bbb4563fb5e60a459c3cb18c0585fced0
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-02-22 11:00:56 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -19,5 +19,5 @@ def main ():
     conf = st.sidebar.slider('Grau de confiança (%)',min_value=80, max_value=99, step=5, format='%i')
     erro = st.sidebar.number_input('Margem de erro (%)', key= int, min_value=5, format='%i')
     st.write("os numeros são ", pop,conf,erro)
-
+main()
 
