ecdb61b9db606aeb52d0e4484e92cc4cab5d3758
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-02-22 19:06:47 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -21,8 +21,12 @@ def main ():
     e = porc(st.sidebar.number_input('Margem de erro (%)', key= float, min_value=1.0, step=0.5, format='%f'))
     p = porc(st.sidebar.number_input('estimativa de verdadeira proporção (%)', key= int, min_value=10, value=50, max_value=100, step=10, format='%i'))
     N = (n*(z**2)*p*(1-p))/((e**2)*(n-1)+(z**2)*p*(1-p))
-    with st.container():
-        st.write("O Calculo do tamanho da amostra ")
-        st.metric(label="Valor aferido", value=f"{N:.2f}", delta="numero de amostras")
+    st.title("O Calculo do tamanho da amostra ")
+    st.metric(label="Valor aferido", value=f"{N:.2f}", delta="numero de amostras")
+    st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
 main()
 
