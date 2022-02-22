d4b887744b9d42539b35a12d9ed567a3f3e1c146
WillianTambori <msn.willian@gmail.com>
2022-02-21 21:56:39 -0300

    Update Estatistica.py

--- Estatistica.py
+++ Estatistica.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-number = st.sidebar.number_input('Insira o numero')
+number = st.sidebar.number_input('Insira o numero',step=int)
 st.write('The numero inserido é ', number)
