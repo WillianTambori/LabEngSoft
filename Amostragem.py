2207bddb6aee24b6af93f16fe69d69eb2a3929ec
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-03-02 13:08:19 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -22,7 +22,7 @@ def main ():
     p = porc(st.sidebar.number_input('Estimativa de verdadeira proporção (%)', key= int, min_value=10, value=50, max_value=100, step=10, format='%i'))
     N = (n*(z**2)*p*(1-p))/((e**2)*(n-1)+(z**2)*p*(1-p))
     st.title("O Cálculo do tamanho da amostra ")
-    st.metric(label="Valor aferido", value=f"{N:.2f}", delta="numero de amostras")
+    st.metric(label="Valor aferido", value=f"{N:.2f}", delta="Numero de amostras")
     st.text("Desenvolvidos por :")
     st.text("Daniele C. S. Turatti –  daniele.turatti@fatec.sp.gov.br – RA1570482111024")
     st.text("Willian Diego Tambori – tambori@fatec.sp.gov.br – RA1570482113002")
