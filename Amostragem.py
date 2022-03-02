467526b9f177f8b902819ab175a1e503ec5c5fbf
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2022-03-02 13:06:25 -0300

    Update Amostragem.py

--- Amostragem.py
+++ Amostragem.py
@@ -17,12 +17,15 @@ def porc(n):
 def main ():
     st.sidebar.title("ADS")
     n =  st.sidebar.number_input('Tamanho da população',key=int, min_value=1, step=1, format='%i')
-    z = nivel(st.sidebar.slider('Nivel de confiança (%)',min_value=80, max_value=99, step=5, format='%i'))
+    z = nivel(st.sidebar.slider('Nível de confiança (%)',min_value=80, max_value=99, step=5, format='%i'))
     e = porc(st.sidebar.number_input('Margem de erro (%)', key= float, min_value=1.0, step=0.5, format='%f'))
-    p = porc(st.sidebar.number_input('estimativa de verdadeira proporção (%)', key= int, min_value=10, value=50, max_value=100, step=10, format='%i'))
+    p = porc(st.sidebar.number_input('Estimativa de verdadeira proporção (%)', key= int, min_value=10, value=50, max_value=100, step=10, format='%i'))
     N = (n*(z**2)*p*(1-p))/((e**2)*(n-1)+(z**2)*p*(1-p))
-    st.title("O Calculo do tamanho da amostra ")
+    st.title("O Cálculo do tamanho da amostra ")
     st.metric(label="Valor aferido", value=f"{N:.2f}", delta="numero de amostras")
+    st.text("Desenvolvidos por :")
+    st.text("Daniele C. S. Turatti –  daniele.turatti@fatec.sp.gov.br – RA1570482111024")
+    st.text("Willian Diego Tambori – tambori@fatec.sp.gov.br – RA1570482113002")
    
 main()
 
